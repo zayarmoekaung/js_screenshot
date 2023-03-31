2cb99ca7ecb6cb9a3316b1c901505e4c4c6470ef
Zayar MoeKaung <zayarmoekaung0@gmail.com>
2023-03-31 10:06:23 +0630

    cnvas_add

--- dist/screenshot.js
+++ dist/screenshot.js
@@ -1,26 +1,61 @@
-var script = document.createElement('script');
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
-var script2 = document.createElement('script');
-script2.src = 'https://html2canvas.hertzen.com/dist/html2canvas.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
-init();
-function init () {
-            const screenshotTarget = document.body;
-            html2canvas(screenshotTarget).then(canvas => {
-            	// to image as png use below line
-                // const base64image = canvas.toDataURL("image/png");
-            	// show the image in window use below line
-                // window.location.href = base64image;
-            	
-            	// screenshot appended to the body as canvas
-                document.body.appendChild(canvas);  
-                dataURL = canvas.toDataURL();  
-                // to print the screenshot in console use below line
-                // console.log(dataURL);
-                
-                // following line is optional and it is to save the screenshot
-                // on the server side. It initiates an ajax call
-                //pushScreenshotToServer(dataURL); 
-            });  
-        }
+// Define the element you want to take a screenshot of
+const element = document.body;
+
+// Create a canvas element and set its dimensions to match the element
+const canvas = document.createElement('canvas');
+canvas.width = element.offsetWidth;
+canvas.height = element.offsetHeight;
+
+// Get the canvas context and draw the element onto the canvas
+const context = canvas.getContext('2d');
+context.drawImage(element, 0, 0);
+
+// Add event listeners to track the mouse movements and clicks
+let startX, startY, endX, endY;
+element.addEventListener('mousedown', handleMouseDown);
+window.addEventListener('mouseup', handleMouseUp);
+
+function handleMouseDown(e) {
+  // Store the starting coordinates of the selection rectangle
+  startX = e.clientX - element.offsetLeft;
+  startY = e.clientY - element.offsetTop;
+
+  // Add event listeners to track the mouse movements
+  element.addEventListener('mousemove', handleMouseMove);
+  window.addEventListener('mousemove', handleMouseMove);
+}
+
+function handleMouseMove(e) {
+  // Update the ending coordinates of the selection rectangle
+  endX = e.clientX - element.offsetLeft;
+  endY = e.clientY - element.offsetTop;
+}
+
+function handleMouseUp() {
+  // Remove the event listeners for mouse movements
+  element.removeEventListener('mousemove', handleMouseMove);
+  window.removeEventListener('mousemove', handleMouseMove);
+
+  // Get the dimensions and position of the selection rectangle
+  const width = Math.abs(endX - startX);
+  const height = Math.abs(endY - startY);
+  const x = Math.min(startX, endX);
+  const y = Math.min(startY, endY);
+
+  // Create a new canvas element with the dimensions of the selection rectangle
+  const selectionCanvas = document.createElement('canvas');
+  selectionCanvas.width = width;
+  selectionCanvas.height = height;
+
+  // Get the canvas context and draw the selected area onto the new canvas
+  const selectionContext = selectionCanvas.getContext('2d');
+  selectionContext.drawImage(canvas, x, y, width, height, 0, 0, width, height);
+
+  // Convert the canvas to a data URL and create an image element
+  const dataUrl = selectionCanvas.toDataURL();
+  const image = new Image();
+  image.src = dataUrl;
+
+  // Add the image to the page or do something else with it
+  document.body.appendChild(image);
+}
